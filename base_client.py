dafab3ec245a3fba1cb5781d43824549b4237393
h.w <811417477@qq.com>
2022-09-21 10:34:06 +0800

    修改弹窗接口

--- base_client.py
+++ base_client.py
@@ -1,4 +1,5 @@
 from plyer import notification
+import win32api,win32con
 
 
 
@@ -13,6 +14,7 @@ class BaseClient(object):
     
     # 输出win弹窗
     def print_win(self, title, notic):
-        notification.notify(title=title,
-                        message=notic,
-                        timeout=self.cfg["check_sh_show_time"])
+        # notification.notify(title=title,
+        #                 message=notic,
+        #                 timeout=self.cfg["check_sh_show_time"])
+        win32api.MessageBox(None, notic, "提醒：", win32con.MB_TOPMOST)
